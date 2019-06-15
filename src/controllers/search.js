b56c2e32b7168dd00a4238c071013a6dc41a0138
何先清 <1678022070@qq.com>
2019-06-15 22:21:20 +0800

    搜索功能基本实现

--- src/controllers/search.js
+++ src/controllers/search.js
@@ -7,16 +7,13 @@ const render = () => {
   $('#index').html(searchTpl)
   //元素聚焦的时候触发
   $('.inputCon').on('focus',showSearchRes)
-  $('.inputCon').on('blur',hideSearchRes)
+  // $('.inputCon').on('blur',hideSearchRes)
   $('.inputCon').on('input',getSearchValue)
-  //输入完成以后
+  //输入完成以后,change事件当标签的值被修改并且失焦后，但并未进行提交
   $('.inputCon').on('change',rendSearchList)
-  $('.sList').on("click","a",function(){
-    console.log($(this))
-    $('.inputCon').val($(this))
-  })
  
   function showSearchRes(){
+   
     $('.searCon').css("display","block")
   }
   function hideSearchRes(){
@@ -30,14 +27,31 @@ const render = () => {
       html += `<li class="sList"><a href="javascript:void(0);">${item}</a></li>`
     })
     $('.resPost').html(html)
+    $('.sList').on("click",function(){
+      console.log(111);
+      console.log($(this))
+      $('.inputCon').val($(this)[0].innerText)
+      hideSearchRes()
+      rendSearchList()
+    })
   }
-  async function rendSearchList(){
-    let valueInp = $('.inputCon').val()
-    let result = await fetch.get(`/api/xhr/search/search.json?keyword=${valueInp}&sortType=0&descSorted=false&categoryId=0&matchType=0&floorPrice=-1&upperPrice=-1&size=40&itemId=0&stillSearch=false&searchWordSource=7&_stat_search=autoComplete`)
-    let data = result.data.directlyList
-    //渲染searchList页面
-    let rendSearchListTpl = template.render(searchListTpl,{data})
-    $('.searlistCon').html(rendSearchListTpl)
+
+  let canRun = true;
+   function rendSearchList(){
+    if(!canRun) return
+    canRun = false
+    //节流，其实这里节流没有意义
+    setTimeout(async function(){
+      let valueInp = $('.inputCon').val()
+      console.log(valueInp);
+      let result = await fetch.get(`/api/xhr/search/search.json?keyword=${valueInp}&sortType=0&descSorted=false&categoryId=0&matchType=0&floorPrice=-1&upperPrice=-1&size=40&itemId=0&stillSearch=false&searchWordSource=7&_stat_search=autoComplete`)
+      let data = result.data.directlyList
+      //渲染searchList页面
+      let rendSearchListTpl = template.render(searchListTpl,{data})
+      // location.href += `?keyword=${valueInp}`
+      $('.searlistCon').html(rendSearchListTpl)
+      canRun = true
+    },100)
   } 
 }
 export default {
